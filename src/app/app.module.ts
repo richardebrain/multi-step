be74ab39dbabb2e3853b89d817b4bd460d8f2e4c
richardebrain <richardebrain20@gmail.com>
2023-06-08 21:44:58 +0100

    feat: side bar design for desktop and page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MultiStepFormComponent } from './components/multi-step-form/multi-step-form.component';
+import { CustomInputComponent } from './components/custom-input/custom-input.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiStepFormComponent,
+    CustomInputComponent,
+    SidebarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
