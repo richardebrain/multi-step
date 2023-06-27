020a03e70272412db30e355bca59ac791c4c2251
richard adebayo <richardebrain20@gmail.com>
2023-06-27 23:32:39 +0100

    finished basic form setup

--- src/app/components/multi-step-form/multi-step-form.component.ts
+++ src/app/components/multi-step-form/multi-step-form.component.ts
@@ -18,12 +18,12 @@ export class MultiStepFormComponent {
   });
   formPlans = this.fb.group({
     plans: ['Arcade', Validators.required],
-    duration: ['monthly'],
+    duration: ['Monthly'],
   });
   formAddOns = this.fb.group({
     addOns: this.fb.group({
-      onlineService: [false],
-      cloudStorage: [false],
+      onlineService: [true],
+      cloudStorage: [true],
       customizableProfile: [false],
     }),
   });
@@ -36,19 +36,19 @@ export class MultiStepFormComponent {
       title: 'Arcade',
       price: '$9/mo',
       img: '../../../assets/images/icon-arcade.svg',
-      duration: 'monthly',
+      duration: 'Monthly',
     },
     {
       title: 'Advanced',
       price: '$12/mo',
       img: '../../../assets/images/icon-advanced.svg',
-      duration: 'monthly',
+      duration: 'Monthly',
     },
     {
       title: 'Pro',
       price: '$15/mo',
       img: '../../../assets/images/icon-pro.svg',
-      duration: 'monthly',
+      duration: 'Monthly',
     },
   ];
   plansDataYearly: PlanTypes[] = [
@@ -56,21 +56,21 @@ export class MultiStepFormComponent {
       title: 'Arcade',
       price: '$90/yr',
       img: '../../../assets/images/icon-arcade.svg',
-      duration: 'yearly',
+      duration: 'Yearly',
       discount: '2 months free',
     },
     {
       title: 'Advanced',
       price: '$120/yr',
       img: '../../../assets/images/icon-advanced.svg',
-      duration: 'yearly',
+      duration: 'Yearly',
       discount: '2 months free',
     },
     {
       title: 'Pro',
       price: '$150/yr',
       img: '../../../assets/images/icon-pro.svg',
-      duration: 'yearly',
+      duration: 'Yearly',
       discount: '2 months free',
     },
   ];
@@ -114,7 +114,55 @@ export class MultiStepFormComponent {
       controlName: 'customizableProfile',
     },
   ];
+  handleChange() {
+    const duration = this.formPlans.value.duration;
+    this.formPlans.controls.duration.setValue(
+      duration === 'Monthly' ? 'Yearly' : 'Monthly'
+    );
+  }
+  updatePlanValue(planTitle: string) {
+    const plansArray =
+      this.formPlans.value.duration === 'Monthly'
+        ? this.plansDataMonthly
+        : this.plansDataYearly;
+    const plan = plansArray.find((plan) => plan.title === planTitle);
+    return plan?.price;
+  }
+  setObjectKeyPairs(obj: any) {
+    return Object.keys(obj).map((key) => ({ [key]: obj[key] }));
+  }
+  updateAddOnsValue() {
+    const formAddOns = this.setObjectKeyPairs(this.formAddOns.value.addOns);
+    const addOnsArray =
+      this.formPlans.value.duration === 'Monthly'
+        ? this.addOnsData
+        : this.addOnsDataYearly;
 
+    const price = addOnsArray
+      .filter((addOn) =>
+        formAddOns.some((formAddOn) => formAddOn[addOn.controlName])
+      )
+      .map((addOn) => ({ title: addOn.title, price: addOn.price }));
+    return price;
+  }
+  isYearlyOrMonthly() {
+    return this.formPlans.controls.duration.value == 'Yearly' ? true : false;
+  }
+  calculateTotal(planTitle: string) {
+    if (!planTitle) return 0;
+    const planPrice = this.updatePlanValue(planTitle)!;
+    const addOnsPrice = this.updateAddOnsValue();
+    const total = addOnsPrice.reduce((acc, curr) => {
+      return acc + parseFloat(curr.price.replace(/\D/g, ''));
+    }, parseInt(planPrice.replace(/\D/g, '')));
+    if (this.isYearlyOrMonthly()) {
+      this.formSummary.controls.summary.setValue('+$' + total + '/yr');
+      return '+$' + total + '/yr';
+    } else {
+      this.formSummary.controls.summary.setValue('+$' + total + '/mo');
+      return '+$' + total + '/mo';
+    }
+  }
   constructor(private fb: FormBuilder) {}
   onSubmit(Formdetails: FormGroup) {
     console.log(Formdetails.value);
